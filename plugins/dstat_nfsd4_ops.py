jcowgill/dstat

commit 56c5306554220989d7bade39924e3bf7be617990

Small fixes to ensure plugins work correctly for Travis

plugins/dstat_nfsd4_ops.py
@@ -36,6 +36,7 @@ class dstat_plugin(dstat):
         self.type = 'd'
         self.width = 5
         self.scale = 1000
+        self.open("/proc/net/rpc/nfsd")
 
     def check(self):
         # other NFS modules had this, so I left it. It seems to work.
@@ -55,17 +56,18 @@ class dstat_plugin(dstat):
                 'layoutcommit', 'layoutget', 'layoutreturn', 'secinfononam', 'sequence',
                 'set_ssv', 'test_stateid', 'want_deleg', 'destroy_clid', 'reclaim_comp'
                 )
-        f_nfs = open("/proc/net/rpc/nfsd")
-        f_nfs.seek(0)
-        for line in f_nfs:
+
+        for line in self.splitlines():
             fields = line.split()
+
             if fields[0] == "proc4ops": # just grab NFSv4 stats
                 assert int(fields[1]) == len(fields[2:]), ("reported field count (%d) does not match actual field count (%d)" % (int(fields[1]), len(fields[2:])))
                 for var in self.vars:
                     self.set2[var] = fields[nfsd4_names.index(var)]
-                
+
         for name in self.vars:
             self.val[name] = (int(self.set2[name]) - int(self.set1[name])) * 1.0 / elapsed
+
         if step == op.delay:
             self.set1.update(self.set2)
 

plugins/dstat_nfsstat4.py
@@ -21,6 +21,7 @@ class dstat_plugin(dstat):
         self.type = 'd'
         self.width = 5
         self.scale = 1000
+        self.open('/proc/net/rpc/nfs')
 
     def check(self):
         # other NFS modules had this, so I left it. It seems to work.
@@ -34,17 +35,17 @@ class dstat_plugin(dstat):
                 "remove", "rename", "link", "symlink", "create", "pathconf", "statfs", "readlink",
                 "readdir", "server_caps", "delegreturn", "getacl", "setacl", "fs_locations",
                 "rel_lkowner", "secinfo")
-        f_nfs = open("/proc/net/rpc/nfs")
-        f_nfs.seek(0)
-        for line in f_nfs:
+        for line in self.splitlines():
+
             fields = line.split()
             if fields[0] == "proc4": # just grab NFSv4 stats
                 assert int(fields[1]) == len(fields[2:]), ("reported field count (%d) does not match actual field count (%d)" % (int(fields[1]), len(fields[2:])))
                 for var in self.vars:
                     self.set2[var] = fields[nfs4_names.index(var)]
-                
+
         for name in self.vars:
             self.val[name] = (int(self.set2[name]) - int(self.set1[name])) * 1.0 / elapsed
+
         if step == op.delay:
             self.set1.update(self.set2)
 

commit 6ec5a4783b4d485f419d492ffcdb192cd6e56f27

Fix test suite failures in NFSv4 plugins
Previously the test suite was failing with:
```
Traceback (most recent call last):
  File "./dstat", line 2846, in <module>
    main()
  File "./dstat", line 2705, in main
    scheduler.run()
  File "/usr/lib/python2.7/sched.py", line 117, in run
    action(*argument)
  File "./dstat", line 2801, in perform
    o.extract()
  File "/tmp/dstat/upstream/plugins/dstat_nfsstat4.py", line 40, in extract
    fields = line.split()
AttributeError: 'list' object has no attribute 'split'
```

This was caused by commit 56c5306554220989d7bade39924e3bf7be617990 where the
NFSv4 plugin was changed to use methods from the dstat class instead of opening
and splitting the file manually. Unfortunately it did not take into account
that dstat.splitlines also splits the fields within a line and the NFSv4 code
tried to do that again. Fix by removing the extra call to split.

Fixes: #136

plugins/dstat_nfsd4_ops.py
@@ -57,9 +57,7 @@ class dstat_plugin(dstat):
                 'set_ssv', 'test_stateid', 'want_deleg', 'destroy_clid', 'reclaim_comp'
                 )
 
-        for line in self.splitlines():
-            fields = line.split()
-
+        for fields in self.splitlines():
             if fields[0] == "proc4ops": # just grab NFSv4 stats
                 assert int(fields[1]) == len(fields[2:]), ("reported field count (%d) does not match actual field count (%d)" % (int(fields[1]), len(fields[2:])))
                 for var in self.vars:

plugins/dstat_nfsstat4.py
@@ -35,9 +35,7 @@ class dstat_plugin(dstat):
                 "remove", "rename", "link", "symlink", "create", "pathconf", "statfs", "readlink",
                 "readdir", "server_caps", "delegreturn", "getacl", "setacl", "fs_locations",
                 "rel_lkowner", "secinfo")
-        for line in self.splitlines():
-
-            fields = line.split()
+        for fields in self.splitlines():
             if fields[0] == "proc4": # just grab NFSv4 stats
                 assert int(fields[1]) == len(fields[2:]), ("reported field count (%d) does not match actual field count (%d)" % (int(fields[1]), len(fields[2:])))
                 for var in self.vars: